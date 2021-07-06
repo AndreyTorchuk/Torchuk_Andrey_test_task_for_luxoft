cd7b4fa635299681a0482f795081c70ada0af696
Andrey <Andrey@DESKTOP-MFL7J4V>
2021-07-06 12:58:40 +0300

    Добавьте файлы проекта.

---
+++ Torchuk_Andrey_test_task/CustomStructure.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Torchuk_Andrey_test_task
+{
+    public class CustomStructure : IEnumerable
+    {
+        SortedDictionary<string, int[]> collection = new SortedDictionary<string, int[]>();
+        public void Add(string key, int[] value)
+        {
+            if (value.Length == 0)
+                return;
+            if (collection.ContainsKey(key))
+            {
+                List<int> list = new List<int>(collection[key]);
+                foreach (int i in value)
+                {
+                    if (!list.Contains(i))
+                        list.Add(i);
+                }
+                list.Sort();
+                collection[key] = list.ToArray();
+            }
+            else
+            {
+                collection.Add(key, value);
+            }
+
+        }
+
+        public void Remove(string key, int[] value)
+        {
+            if (value.Length == 0)
+                return;
+            if (collection.ContainsKey(key))
+            {
+                List<int> list = new List<int>(collection[key]);
+                foreach (int i in value)
+                {
+                    list.Remove(i);
+                }
+                if (list.Count == 0)
+                {
+                    collection.Remove(key);
+                }
+                else
+                {
+                    list.Sort();
+                    collection[key] = list.ToArray();
+                }
+            }
+        }
+        public void Merge(CustomStructure source)
+        {
+            foreach (KeyValuePair<string, int[]> i in source)
+            {
+                Add(i.Key, i.Value);
+            }
+        }
+
+        public void Cut(CustomStructure source)
+        {
+            foreach (KeyValuePair<string, int[]> i in source)
+            {
+                Remove(i.Key, i.Value);
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return collection.GetEnumerator();
+        }
+    }
+}
---
+++ Torchuk_Andrey_test_task/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Torchuk_Andrey_test_task
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            SolutionTest tests = new SolutionTest();
+            tests.ExampleTestMerge();
+            tests.ExampleTestCut();
+            tests.MyTestEmpty();
+        }
+    }
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void ExampleTestMerge()
+        {
+            CustomStructure target = new CustomStructure();
+            target.Add("collection1", new int[] { 1, 2, 4, 5 });
+            target.Add("collection2", new int[] { 1, 2, 3, 4, 5 });
+            target.Add("collection4", new int[] { 4, 5 });
+            CustomStructure source = new CustomStructure();
+            source.Add("collection1", new int[] { 2, 3, 5, 6, 2, 10 });
+            source.Add("collection2", new int[] { 2, 4, 5, 20 });
+            source.Add("collection3", new int[] { 4, 5 });
+            source.Add("collection4", new int[] { 4, 5 });
+            CustomStructure expected = new CustomStructure();
+            expected.Add("collection1", new int[] { 1, 2, 3, 4, 5, 6, 10 });
+            expected.Add("collection2", new int[] { 1, 2, 3, 4, 5, 20 });
+            expected.Add("collection3", new int[] { 4, 5 });
+            expected.Add("collection4", new int[] { 4, 5 });
+
+            target.Merge(source);
+            Assert.AreEqual(expected, target);
+        }
+        [Test]
+        public void ExampleTestCut()
+        {
+            CustomStructure target = new CustomStructure();
+            target.Add("collection1", new int[] { 1, 2, 4, 5 });
+            target.Add("collection2", new int[] { 1, 2, 3, 4, 5 });
+            target.Add("collection4", new int[] { 4, 5 });
+            CustomStructure source = new CustomStructure();
+            source.Add("collection1", new int[] { 2, 3, 5, 6, 2, 10 });
+            source.Add("collection2", new int[] { 2, 4, 5, 20 });
+            source.Add("collection3", new int[] { 4, 5 });
+            source.Add("collection4", new int[] { 4, 5 });
+            CustomStructure expected = new CustomStructure();
+            expected.Add("collection1", new int[] { 1, 4 });
+            expected.Add("collection2", new int[] { 1, 3 });
+
+            target.Cut(source);
+            Assert.AreEqual(expected, target);
+        }
+        [Test]
+        public void MyTestEmpty()
+        {
+            CustomStructure target = new CustomStructure();
+            target.Add("collection1", new int[] {});
+            target.Add("collection2", new int[] { 1, 2, 3, 4, 5 });
+            CustomStructure source = new CustomStructure();
+            source.Add("collection1", new int[] { 1, 2, 3, 4, 5 });
+            source.Add("collection2", new int[] {});
+            CustomStructure emptySource = new CustomStructure();
+            emptySource.Add("collection1", new int[] { });
+            emptySource.Add("collection2", new int[] { });
+            CustomStructure expected = new CustomStructure();
+            expected.Add("collection1", new int[] { 1, 2, 3, 4, 5 });
+            expected.Add("collection2", new int[] { 1, 2, 3, 4, 5 });
+
+            target.Merge(source);
+            Assert.AreEqual(expected, target);
+            target.Cut(emptySource);
+            Assert.AreEqual(expected, target);
+        }
+    }
+}
